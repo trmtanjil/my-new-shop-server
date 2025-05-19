7f5d99008fae90f868c7200948b41bce5aab46d8
Tanjilur <trmtanjil02@gmail.com>
2025-05-19 14:37:01 +0600

    server data add get all data and data delet commit

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const port =process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri =`mongodb+srv://${process.env.SHOP_USER}:${process.env.SHOP_PASS}@trmcamp0.7libfgs.mongodb.net/?retryWrites=true&w=majority&appName=trmcamp0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCllection = client.db('productdb').collection('products')
+
+    // get all data 
+    app.get('/products',async(req,res)=>{
+        const result = await productCllection.find().toArray();
+        res.send(result)
+    })
+
+    // server data add 
+    app.post('/products',async(req,res)=>{
+         const newProduct = req.body
+         const result = await productCllection.insertOne(newProduct);
+         res.send(result)
+        })
+        
+
+        // server data delet 
+    app.delete('/products/:id',async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCllection.deleteOne(query)
+        res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
